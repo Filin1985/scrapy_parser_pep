1023972c9d7ec613bd268c87def3508ca8c3e538
Marat Ikhsanov <ihsmen@gmail.com>
2023-03-25 18:17:07 +0300

    fix: Исправить замечания после первого ревью

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -3,12 +3,15 @@ import csv
 import datetime as dt
 from pathlib import Path
 
+from settings import RESULTS_DIR
+
 
 BASE_DIR = Path(__file__).parent
 
-RESULTS_DIR = 'results'
+
 DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
 TITLE_NAMES = ['Статус', 'Количество']
+TOTAL_NAME = 'Total'
 PREFIX_NAME = 'status_summary_{}.csv'
 
 
@@ -17,21 +20,20 @@ class PepParsePipeline:
     def __init__(self):
         self.results_dir = BASE_DIR / RESULTS_DIR
         self.results_dir.mkdir(exist_ok=True)
-        self.status_counts = defaultdict(int)
 
     def open_spider(self, spider):
-        time = dt.datetime.now().strftime(DATETIME_FORMAT)
-        file_path = self.results_dir / PREFIX_NAME.format(time)
-        self.output_file = csv.writer(
-            open(file_path, 'w')
-        )
-        self.output_file.writerow(TITLE_NAMES)
+        self.status_counts = defaultdict(int)
 
     def process_item(self, item, spider):
         self.status_counts[item['status']] += 1
         return item
 
     def close_spider(self, spider):
-        print(self.status_counts)
-        self.status_counts['Total'] = sum(self.status_counts.values())
+        time = dt.datetime.now().strftime(DATETIME_FORMAT)
+        file_path = self.results_dir / PREFIX_NAME.format(time)
+        self.output_file = csv.writer(
+            open(file_path, 'w')
+        )
+        self.output_file.writerow(TITLE_NAMES)
+        self.status_counts[TOTAL_NAME] = sum(self.status_counts.values())
         self.output_file.writerows(self.status_counts.items())
--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -5,30 +5,27 @@ import scrapy
 from pep_parse.items import PepParseItem
 
 SEARCH_PATTERN = r'PEP\s(?P<number>\d+)\W+(?P<name>.+)$'
+URL_NAME = 'peps.python.org'
 
 
 class PepSpider(scrapy.Spider):
     name = 'pep'
-    allowed_domains = ['peps.python.org']
-    start_urls = ['https://peps.python.org/']
+    allowed_domains = [URL_NAME]
+    start_urls = [f'https://{url}/' for url in allowed_domains]
 
     def parse(self, response):
         for section_link in response.css(
             '#numerical-index tr a[href]'
         ):
-            try:
-                yield response.follow(section_link, callback=self.parse_pep)
-            except AttributeError:
-                continue
+            yield response.follow(section_link, callback=self.parse_pep)
 
     def parse_pep(self, response):
         header = response.css('h1.page-title::text').get()
         number, name = re.search(SEARCH_PATTERN, header).groups()
-        data = {
+        yield PepParseItem({
             'number': number,
             'name': name,
             'status': response.css(
                 'dt:contains("Status") + dd > abbr::text'
             ).get()
-        }
-        yield PepParseItem(data)
+        })
